387c00120c632f741921f29f196f87eca81b3332
Syrian Watermelon <batikhsouri@gmail.com>
2014-02-01 23:34:10 +0100

    Backup system nearly complete

--- backup-job.js
+++ backup-job.js
@@ -19,8 +19,8 @@ if (os.platform().toString().toLowerCase().indexOf('win') > -1){
 	folderSeperator = '/';
 }
 
+//Replacing the current accessToken by an other one from the DB
 function refreshToken(){
-	//Replacing the current accessToken by an other one from the DB
 	FbUser.find(function(err, users){
 		if (err){
 			console.log('Error while changing access token:\n' + err);
@@ -34,8 +34,25 @@ function refreshToken(){
 }
 refreshToken();
 
+//Refreshing feeds' metadata
+function refreshMetadata(){
+	Feed.find(function(err, feeds){
+		if (err){
+			console.log('Error while trying to reload feeds metadata:\n' + err);
+			return;
+		}
+		if (!(feeds && feeds.length > 0)) return;
+		for (var i = 0; i < feeds.length; i++){
+			fbgraph.get(feeds.id, {fields: 'id,name,link,picture'}, function(err, fbRes){
+				Feed.update({id: feeds.id}, {name: fbRes.name, }).exec();
+			});
+		}
+	});
+}
+
 var validFbPaths = ['http://facebook.com', 'https://facebook.com', 'http://www.facebook.com', 'https://www.facebook.com', 'http://m.facebook.com', 'https://m.facebook.com'];
 
+//Checking whether the given path is a Facebook url
 function isFbUrl(path){
 	if (typeof path != 'string') throw new TypeError('path must be a string');
 	for (var i = 0; i < validFbPaths.length; i++){
@@ -44,6 +61,7 @@ function isFbUrl(path){
 	return false;
 }
 
+//Getting the page name (vanity and non-vanity)
 function getFbPath(path, removeEdges){
 	if (typeof path != 'string') throw new TypeError('path must be a string');
 	for (var i = 0; i < validFbPaths.length; i++){
@@ -51,8 +69,10 @@ function getFbPath(path, removeEdges){
 			path = path.replace(validFbPaths[i], '');
 			if (path.indexOf('/pages/') == 0){ // Taking the Page-Name from https://facebook.com/pages/Page-Name/batikhNumber (when a page doesn't have a vanity name)
 				path = path.replace('/pages/', '');
-				var batikhNumberLocation = path.indexOf('/');
-				path = path.substring(0, batikhNumberLocation);
+				if (removeEdges){
+					var batikhNumberLocation = path.indexOf('/');
+					path = path.substring(0, batikhNumberLocation);
+				}
 			}
 			return path;
 		}
@@ -60,14 +80,46 @@ function getFbPath(path, removeEdges){
 	throw new TypeError('The given path isn\'t from facebook');
 }
 
-function navigatePage(pageId, until, cb){
+//Getting all the posts, with an optional interval (since or until parameter)
+function navigatePage(pageId, until, since, cb){
 	if (typeof pageId != 'string') throw new TypeError('pageId must be a string');
-	if (until && !(typeof until == 'number' || typeof until == 'date')) throw new TypeError('When defined, "until" must be a number or a date');
+	if (typeof until != 'undefined' && !(typeof until == 'number' || typeof until == 'date')) throw new TypeError('When defined, "until" must be a number or a date');
+	if (typeof since != 'undefined' && !(typeof since == 'number' || typeof since == 'date')) throw new TypeError('When defined, "since" must be a number or a date');
 	if (cb && typeof cb != 'function') throw new TypeError('When defined, "cb" must be a function');
-	var reqText = pageId + '/posts/';
-	if (until)  
+	if (typeof until != 'undefined' && typeof since != 'since') throw new TypeError('You can use only one time pagination parameter at a time');
+	var reqText = pageId + '/posts';
+
+	function fbGet(path, until, since){
+		var options = {};
+		if (typeof until == 'date') options.until = until.getTime();
+		if (typeof until == 'number') options.until = until;
+		if (typeof since == 'date') options.since = since.getTime();
+		if (typeof since == 'number') options.since = since;
+		fbgraph.get(path, options, function(err, fbRes){
+			if (err) {
+				console.log('Error while getting updates from : ' + pageId + '\n' + err);
+				if (cb) cb();
+				return;
+			}
+			if (!fbRes.data){ //If no error and no data was returned, then end of feed (or whatever)
+				if (cb) cb();
+				return;
+			}
+			for (var i = 0; i < fbRes.data.length; i++){
+				backupFbPost(fbRes.data[i]);
+			}
+			if (fbRes.paging && fbRes.paging.next){
+				fbGet(fbRes.paging.next);
+			} else {
+				if (cb) cb();
+			}
+		})
+	}
+
+	fbGet(reqText);
 }
 
+//Saving a single fb post on the server
 function backupFbPost(postObj){
 	if (typeof postObj !== 'object') throw new TypeError('postObj must be an object');
 	var feedId = postObj.from.id;
@@ -96,23 +148,33 @@ function backupFbPost(postObj){
 
 module.exports = {};
 
-exports.launchFeedBackup = function(feedId){
-	Feed.count({id: feedId}, function(err, feedCount){
+//Launching a feed backup process
+exports.launchFeedBackup = function(feedId, callback){
+	if (callback && typeof callback != 'function') throw new TypeError('');
+	Feed.findOne({id: feedId}, function(err, _feed){
 		if (err){
 			console.log('Error when checking if a certain feed exists:\n' + err);
 			return;
 		}
-
+		if (_feed){
+			console.log('Backing up : "' + _feed.name + '"');
+			navigatePage(_feed.id, undefined, _feed.lastBackup, function(){
+				console.log('Backup finished : "' + _feed.name + "'");
+			});
+		} else {
+			if (callback) callback(false);
+		}
 	});
 };
 
+//Adding a feed a that will be backed up by the system
 exports.addFeed = function(feedUrl){
 	if (!isFbUrl(feedUrl)) throw new TypeError('As of now, only FB pages are supported');
 	var fbPath = getFbPath(feedUrl);
 	if (fbPath.lastIndexOf('/') != fbPath.length - 1){
 		fbPath += '/';
 	}
-	fbPath += '?fields=id,name,picture';
+	fbPath += '?fields=id,name,link,picture';
 	fbgraph.get(fbPath, function(err, res){
 		if (err){
 			console.log('Error when getting info of: ' + fbPath + '\n' + err);
@@ -122,10 +184,29 @@ exports.addFeed = function(feedUrl){
 			id: res.id,
 			name: res.name,
 			type: 'fbpage',
-			url: feedUrl,
-			profileImage: res.data.url
+			url: res.link,
+			profileImage: res.picture.data.url
 		});
 		newFeed.save();
 		exports.launchFeedBackup(res.id);
 	});
+};
+
+var backupInterval;
+var reloadFeedMetadataInterval;
+
+//Creating the auto backup job
+exports.start = function(){
+	reloadFeedMetadataInterval = setInterval(config.metadataRefreshInterval, function(){
+
+	});
+	backupInterval = setInterval(config.postsBackupInterval, function(){
+
+	});
+};
+
+//Stopping the backup process
+exports.stop = function(){
+	clearInterval(reloadFeedMetadataInterval);
+	clearInterval(backupInterval);
 };
