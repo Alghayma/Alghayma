b896909cfddccead39628c07f06b2003fb6004b8
Syrian Watermelon <batikhsouri@gmail.com>
2014-02-04 22:21:59 +0000

    Adding example config

---
+++ config-example.js
@@ -0,0 +1,13 @@
+module.exports = {
+	dbhost: 'localhost',
+	dbport: 27017,
+	dbname: 'alghayma',
+	dbuser: undefined,
+	dbpass: undefined,
+	httpport: 80,
+	fbappid: '',
+	fbapptoken: '',
+	mediafolder: './backupmedia/',
+	postsBackupInterval: 7200000, //2h
+	metadataRefreshInterval: 86400000 //24h
+};
