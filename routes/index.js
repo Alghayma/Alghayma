dff059a2c4604ff254a9ccae7ef4b3685e4d5134
Frederic Jacobs <github@fredericjacobs.com>
2014-02-10 22:50:14 +0000

    Revert 79ec982..aa737cf
    This rolls back to commit 79ec9824bdd1bb3d87ac6326436f4cf01b82ce2f.

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,3 @@ var mongoose = require('mongoose');
 exports.index = function(req, res){
 	res.render('index', { title: 'Alghayma' });
 };
-
-exports.validator = function(req, res){
-	// Iterate through each validator, return possible choices.
-	
-	res.send();
-}
