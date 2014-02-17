2db1e331b73a0959f06938b384aecb4aef2f0943
Frederic Jacobs <github@fredericjacobs.com>
2014-02-17 21:42:31 +0100

    Deploy Scripts, a start

---
+++ deployement/watcher.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var app = express();
+var forever = require('forever-monitor');
+var shell = require('shelljs');
+var githubSourceSubnet = "192.30.252.0/22";
+var execPath = process.cwd();
+var path = require('path')
+var fs = require('fs');
+
+var repoRootPath = path.join(execPath, "..");
+var pathToLogs = path.join(repoRootPath, "logs")
+
+var mainInstance = undefined;
+var queue = undefined;
+
+// Initialization - Make dir for logging
+
+if (!fs.existsSync(pathToLogs)) {
+	fs.mkdirSync(pathToLogs);
+}
+
+app.use(express.bodyParser());
+app.listen(3002);
+
+app.post('/deploy', function(req, res){
+	if (inSubnet(req.connection.remoteAddress, githubSourceSubnet)){
+		if(req.body.ref === "refs/heads/production"){
+			console.log("Time to deploy a new instance")
+		}
+	}
+	else{
+		console.log("We got a deploy request from someone who wasn't GitHub");
+	}
+});
+
+// Methods to deal with subnet verification
+
+var ip2long = function(ip){
+    var components;
+
+    if(components = ip.match(/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/))
+    {
+        var iplong = 0;
+        var power  = 1;
+        for(var i=4; i>=1; i-=1)
+        {
+            iplong += power * parseInt(components[i]);
+            power  *= 256;
+        }
+        return iplong;
+    }
+    else return -1;
+};
+
+var inSubNet = function(ip, subnet)
+{   
+    var mask, base_ip, long_ip = ip2long(ip);
+    if( (mask = subnet.match(/^(.*?)\/(\d{1,2})$/)) && ((base_ip=ip2long(mask[1])) >= 0) )
+    {
+        var freedom = Math.pow(2, 32 - parseInt(mask[2]));
+        return (long_ip > base_ip) && (long_ip < base_ip + freedom - 1);
+    }
+    else return false;
+};
+
+function gitPull(root, options)
+{
+
+    var cmd = 'git pull --rebase';
+	shell.cd(root);
+    shell.exec(cmd, function(code, output) {
+    	console.log(cmd + ' exited with code ' + code);
+
+    	// Awesome, we checked out the new changes. Let's now restart the instances!
+
+    	mainInstance = new (forever.Monitor)("app.js", {
+    		'silent': true,
+    		'killTree': true,
+    		'sourceDir': repoRootPath,
+    		'watch': false,
+    		'logFile': path.join(pathToLogs, "mainDeamon.log"),
+    		'outFile': path.join(pathToLogs, "mainOut.log"),
+    		'errFile': path.join(pathToLogs, "mainError.log")
+    	});
+    	queue = new (forever.Monitor)("worker.js", {
+    		'silent': true,
+    		'killTree': true,
+    		'sourceDir': repoRootPath,
+    		'watch': false,
+    		'logFile': path.join(pathToLogs, "queueDeamon.log"),
+    		'outFile': path.join(pathToLogs, "queueOut.log"),
+    		'errFile': path.join(pathToLogs, "queueError.log")
+    	});
+
+    	mainInstance.start();
+    	queue.start();
+
+    });
+
+   
+}
+
+function deploy (){
+	if (mainInstance) {
+		mainInstance.stop();
+	}
+	if (queue) {
+		queue.stop();
+	}
+
+	// Both instances are stopped. Let's now proceed to the git pull 
+
+	gitPull(repoRootPath)
+
+}
+
+deploy()
