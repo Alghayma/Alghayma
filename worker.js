233f44a8b1adcdb94d54b52a24f69e1c5243d099
Frederic Jacobs <github@fredericjacobs.com>
2014-02-19 00:43:25 +0100

    If we have issues with Redis, we can just reschedule everything in a queue

--- worker.js
+++ worker.js
@@ -8,28 +8,54 @@ var config = require(path.join(process.cwd(), "config"));
 var fbBgWorker = require(path.join(process.cwd(), 'extensions', 'Facebook', 'backgroundJob'));
 var net = require('net');
 var noop = function() {};
+var remakeJobQueue = false;
 
 if (cluster.isMaster) {
-  var reschedule = function( err, ids ){
-    ids.forEach( function( id ){
-      kue.Job.get( id, function(err, failedJob){
-        if (err) {
-          console.log("An error occured while retreiving a failed job : "+ err);
-        } else{
-          failedJob.remove(function(err){
-            if (err) {
-              console.log("An error occured while removing a failed job : "+ err);
-            } else{
-              jobs.create('facebookJob', {title: "Backup of " + failedJob.data.feed.name, feed: failedJob.data.feed}).priority('high').save();
-            }
-          });
-        }
-      });
+  
+  var clearJobs = function(err, ids){
+    ids.forEach(function(id){
+      kue.Job.get(id, function(err, aJob){
+        if (err) {console.log("Couldn't get completed job because : " + err)}
+        aJob.remove(function(err){if (err) {"Failed to delete completed job"}});
+      })
     });
-  };
+  }
 
-  jobs.active(reschedule);
-  jobs.failed(reschedule);
+  if (remakeJobQueue) {
+    jobs.active(clearJobs);
+    jobs.failed(clearJobs);
+    jobs.inactive(clearJobs);
+
+    // Now let's get all the feeds of the DB and launch jobs for them
+
+    fbBgWorker.scheduleAllFeeds(jobs);
+
+  } else {
+    var reschedule = function( err, ids ){
+      ids.forEach( function( id ){
+        kue.Job.get( id, function(err, failedJob){
+          if (err) {
+            console.log("An error occured while retreiving a failed job : "+ err);
+          } else{
+            failedJob.remove(function(err){
+              if (err) {
+                console.log("An error occured while removing a failed job : "+ err);
+              } else{
+                jobs.create('facebookJob', {title: "Backup of " + failedJob.data.feed.name, feed: failedJob.data.feed}).priority('high').save();
+              }
+            });
+          }
+        });
+      });
+    };
+
+    jobs.active(reschedule);
+    jobs.failed(reschedule);
+  }
+
+  // Clean completed job queue 
+
+  jobs.complete(clearJobs);
 
   for (var i = 0; i < numCPUs; i++) {
     cluster.fork();
@@ -65,111 +91,7 @@ if (cluster.isMaster) {
     } , 600000);
   });
 
-  // Queue cleanup
-
-  jobs.CLEANUP_MAX_FAILED_TIME = 30 * 24 * 60 * 60 * 1000;  // 30 days
-  jobs.CLEANUP_MAX_ACTIVE_TIME = 1 * 24 * 60 * 60 * 1000;  // 1 day
-  jobs.CLEANUP_MAX_COMPLETE_TIME = 5 * 24 * 60 * 60 * 1000; // 5 days
-
-  // this is a simple log action
-  function QueueActionLog(message) {
-    this.message = message || 'QueueActionLog :: got an action for job id(%s)';
-
-    this.apply = function(job) {
-      console.log(util.format(this.message, job.id));
-      return true;
-    };
-  }
-
-  // remove item action
-  function QueueActionRemove(age) {
-    this.age = age;
-
-    this.apply = function(job) {
-      job.remove(noop);
-      return true;
-    };
-  }
-
-  function QueueFilterAge(age) {
-      this.now = new Date().getTime();
-      this.age = age;
-
-      this.test = function(job) {
-          var created = parseInt(job.created_at);
-          var age = this.now - created;
-          return age > this.age;
-      };
-   }
-
-  // the queue iterator
-  var queueIterator = function(ids, queueFilterChain, queueActionChain) {
-    ids.forEach(function(id, index) {
-      // get the kue job
-      kue.Job.get(id, function(err, job) {
-        if (err || !job) return;
-        var filterIterator = function(filter) { return filter.test(job) };
-        var actionIterator = function(filter) { return filter.apply(job) };
-
-        // apply filter chain
-        if(queueFilterChain.every(filterIterator)) {
-
-          // apply action chain
-          queueActionChain.every(actionIterator);
-        }
-      });
-    });
-  };
-
-  function performCleanup() {
-    var ki = new kue;
-
-    // ki.failed(function(err, ids) {
-    //   if (!ids) return;
-    //   queueIterator(
-    //     ids,
-    //     [new QueueFilterAge(jobs.CLEANUP_MAX_FAILED_TIME)],
-    //     [new QueueActionLog('Going to remove job id(%s) for being failed too long'),
-    //       new QueueActionRemove()]
-    //   );
-    // });
-
-    // ki.active(function(err, ids) {
-    //   if (!ids) return;
-    //   queueIterator(
-    //     ids,
-    //     [new QueueFilterAge(jobs.CLEANUP_MAX_ACTIVE_TIME)],
-    //     [new QueueActionLog('Going to remove job id(%s) for being active too long'),
-    //       new QueueActionRemove()]
-    //   );
-    // });
-
-    ki.complete(function(err, ids) {
-      if (!ids) return;
-      queueIterator(
-        ids,
-        [new QueueFilterAge(jobs.CLEANUP_MAX_COMPLETE_TIME)],
-        [new QueueActionLog('Going to remove job id(%s) for being complete too long'),
-          new QueueActionRemove()]
-      );
-    });
-  }
-
-  function rebuildAllQueue(){
-
-  }
-
-  function clearJobs(){
-    // Cleanup removes old completed jobs
-    performCleanup();
-  }
-
-  function promoteDelayed(){
-    jobs.promote();
-  }
-
-  clearJobs();
-  promoteDelayed();
+  jobs.promote();
 
 } else {
   
