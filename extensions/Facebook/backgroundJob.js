69b622391409a96abd010de65de8f6e45ba8ef99
Syrian Watermelon <batikhsouri@gmail.com>
2014-03-05 09:15:03 +0100

    You have to execute the callback -_-

--- extensions/Facebook/backgroundJob.js
+++ extensions/Facebook/backgroundJob.js
@@ -139,7 +139,7 @@ function navigatePage (pageId, Until, Since, cb, job, done) {
         if (err.code == 1 || err.code == 2){ //Internal FB errors
           job.log(JSON.stringify(err));
           job.log('Gonna reschedule that job');
-          if (cb) cb;
+          if (cb) cb();
           return;
         } else if (err.code == 17){
           job.log("Hitting the maximum rate limit " + JSON.stringify(err));
